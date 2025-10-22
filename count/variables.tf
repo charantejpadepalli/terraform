53ee6d55bddb2400dfb73065d5e2d614e5caa3e3
charantej Padepalli <cherry18tejs@gmail.com>
2025-10-22 22:14:07 +0530

    Terraform

---
+++ count/variables.tf
@@ -0,0 +1,9 @@
+variable "instances" {
+  default = ["mongodb", "redis", "mysql", "rabbitmq" ]
+}
+variable "zone_id" {
+  default = "Z07634881IMZZFH5TBV0T"
+}
+variable "domain_name" {
+  default = "devopspractice.shop"
+}
