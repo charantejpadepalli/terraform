121310dbef84a3cd8e6e109bb339f2924edf173f
charantej Padepalli <cherry18tejs@gmail.com>
2025-10-23 13:20:46 +0530

    Terraform

--- locals/provider.tf
+++ locals/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {
     bucket = "devopspractice-terraform"
-    key    = "remote-state-demo"
+    key    = "locals-demo"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
